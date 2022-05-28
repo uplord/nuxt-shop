5beca691178dfd850d71f2eae333b658f3701b62
Michael Allen <michael@Michaels-MacBook-Pro.local>
2022-05-28 23:13:34 +0100

    test 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
         },
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
             { hid: 'description', name: 'description', content: '' },
             { name: 'format-detection', content: 'telephone=no' },
             { name: 'color-scheme', content: 'light' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="">
-        <h1>Home</h1>
-        <div class="button-group">
-            <a href="/checkout" class="button">Checkout</a>
-        </div>
+        <section>
+            <div class="container">
+                <h1>Home</h1>
+                <div class="button-group">
+                    <a href="/checkout" class="button">Checkout</a>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
