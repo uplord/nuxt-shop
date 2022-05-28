7bd9611b9a468e118929bf851b9012efa6bbd676
Michael Allen <michael@Michaels-MacBook-Pro.local>
2022-05-28 23:35:45 +0100

    test 3

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="">
-        <h1>Checkout</h1>
+        <section>
+            <div class="container">
+                <h1>Checkout</h1>
+                <div class="button-group">
+                    <a href="/" class="button">Checkout</a>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
