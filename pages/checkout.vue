e1d211ab8e2896742a2d57a9182c24143e60abf5
Michael Allen <michael@Michaels-MacBook-Pro.local>
2022-05-29 01:02:15 +0100

    test 4

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -2,16 +2,64 @@
     <div class="">
         <section>
             <div class="container">
-                <h1>Checkout</h1>
-                <div class="button-group">
-                    <a href="/complete" class="button">Complete</a>
-                </div>
+                <h2>Redirect example Test</h2>
+                <ul style="margin-left: 0; padding-left: 15px;">
+                <li>
+                    <a href="exp://REPLACE_ME/">
+                    Go back with no params
+                    </a>
+                </li>
+                <li>
+                    <a href="exp://REPLACE_ME/message=Hello%20World">
+                    Pass back a message of "hello world"
+                    </a>
+                </li>
+                <li>
+                    <a href="exp://REPLACE_ME/authToken=23xbdbb21b3">
+                    Pass back some fake auth token
+                    </a>
+                </li>
+                </ul>
+
+                <p>
+                Redirecting automatically back to app in <span class="countdown">5</span> seconds
+                </p>
             </div>
         </section>
     </div>
 </template>
 
 <script>
+document.addEventListener("DOMContentLoaded", function(event) {
+var links = document.querySelectorAll('a');
+var baseUri = 'exp://127.0.0.1:19000';
+
+// Take the uri from the params
+var qs = decodeURIComponent(document.location.search);
+if (qs) {
+    baseUri = qs.split('?linkingUri=')[1];
+}
+
+// Update the link urls
+for (var i = 0; i < links.length; ++i) {
+    links[i].href = links[i].href.replace('exp://REPLACE_ME/', baseUri);
+    console.log(links[i].href);
+}
+
+var redirectInterval = setInterval(function() {
+    var countdown = document.querySelector('.countdown');
+    var t = parseInt(countdown.innerText, 10);
+    t -= 1;
+
+    countdown.innerText = t;
+
+    if (t === 0) {
+    clearInterval(redirectInterval);
+    window.location.href = baseUri + 'message=' + encodeURIComponent('Redirected automatically by timer');
+    }
+}, 1000);
+});
+
 export default {
     name: 'CheckoutPage'
 }
