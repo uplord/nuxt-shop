6e88bea5f0a6b0b14fd8f844afacc09e6410ddbc
Michael Allen <michael@Michaels-MacBook-Pro.local>
2022-05-29 01:04:40 +0100

    test 4

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -30,37 +30,40 @@
 </template>
 
 <script>
-document.addEventListener("DOMContentLoaded", function(event) {
-var links = document.querySelectorAll('a');
-var baseUri = 'exp://127.0.0.1:19000';
-
-// Take the uri from the params
-var qs = decodeURIComponent(document.location.search);
-if (qs) {
-    baseUri = qs.split('?linkingUri=')[1];
-}
-
-// Update the link urls
-for (var i = 0; i < links.length; ++i) {
-    links[i].href = links[i].href.replace('exp://REPLACE_ME/', baseUri);
-    console.log(links[i].href);
-}
-
-var redirectInterval = setInterval(function() {
-    var countdown = document.querySelector('.countdown');
-    var t = parseInt(countdown.innerText, 10);
-    t -= 1;
-
-    countdown.innerText = t;
-
-    if (t === 0) {
-    clearInterval(redirectInterval);
-    window.location.href = baseUri + 'message=' + encodeURIComponent('Redirected automatically by timer');
-    }
-}, 1000);
-});
 
 export default {
-    name: 'CheckoutPage'
+    name: 'CheckoutPage',
+    mounted () {
+        document.addEventListener("DOMContentLoaded", function(event) {
+        var links = document.querySelectorAll('a');
+        var baseUri = 'exp://127.0.0.1:19000';
+
+        // Take the uri from the params
+        var qs = decodeURIComponent(document.location.search);
+        if (qs) {
+            baseUri = qs.split('?linkingUri=')[1];
+        }
+
+        // Update the link urls
+        for (var i = 0; i < links.length; ++i) {
+            links[i].href = links[i].href.replace('exp://REPLACE_ME/', baseUri);
+            console.log(links[i].href);
+        }
+
+        var redirectInterval = setInterval(function() {
+            var countdown = document.querySelector('.countdown');
+            var t = parseInt(countdown.innerText, 10);
+            t -= 1;
+
+            countdown.innerText = t;
+
+            if (t === 0) {
+            clearInterval(redirectInterval);
+            window.location.href = baseUri + 'message=' + encodeURIComponent('Redirected automatically by timer');
+            }
+        }, 1000);
+        });
+
+    }
 }
 </script>
