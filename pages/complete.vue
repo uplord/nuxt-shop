d7c4e5121de69a6f634e5da72fb32e49f84662c7
Michael Allen <michael@Michaels-MacBook-Pro.local>
2022-05-29 10:33:46 +0100

    header

--- pages/complete.vue
+++ pages/complete.vue
@@ -6,7 +6,7 @@
                 <div class="container">
                     <h1>Order Complete</h1>
                     <div class="button-group">
-                        <a href="exp://192.168.0.12:19000?sucess=checkout" class="button">Close Checkoit</a>
+                        <a href="exp://192.168.0.12:19000?success=checkout" class="button">Close Checkoit</a>
                     </div>
                 </div>
             </section>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
     <div id="page">
         <Header/>
+        <div id="content">
+            <section>
+                <div class="container">
+                    <h1>Home</h1>
+                </div>
+            </section>
+        </div>
         <Footer/>
         <NavigationTabs/>
     </div>
